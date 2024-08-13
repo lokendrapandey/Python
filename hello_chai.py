c6a1652e95eb3d6d87e7725b6ec956cb6d4e5bc4
lokendra pandey <lokendrapandey5278@gmail.com>
2024-08-13 17:01:02 +0530

    list

--- hello_chai.py
+++ hello_chai.py
@@ -6,3 +6,43 @@ def chai(n):
 
 chai("shivam")
 
+chai_one = "lemaon tea"
+chai_two = "ginger tea"
+chai_three = "masala tea"
+
+a = 3
+# print(a);
+a = 'lokendra'
+print(a);
+
+string = "shivam pandey a a";
+# print(string.count("a"));
+chai_variety = ["lemon", "masala", "ginger"];
+
+# print(chai_variety);
+# print(" - ".join(chai_variety))
+# print("=".join(chai_variety))
+# print(" , " .join(chai_variety))
+# print("/".join(chai_variety))
+
+# print(len(string));
+
+# for letter in string:
+#     print(letter)
+
+variety = ["lemon", "masala", "ginger"];
+print(variety[-1]);
+print(variety[:2]);
+print(variety[1:2]);
+print(variety[2])
+variety[2] = "herbal"
+print(variety)
+for tea in variety:
+   print(tea, end="-")
+
+variety.append("gggg")
+if "lemon" in variety:
+    print("hena ")
+
+print(variety)
+ 
